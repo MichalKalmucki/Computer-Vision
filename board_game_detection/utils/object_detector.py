a1ee223e585ef79e7580a3e7f9126f8243ab15eb
myszjordjot <116115347+myszjordjot@users.noreply.github.com>
2024-01-18 22:39:33 +0100

    naming

--- board_game_detection/utils/object_detector.py
+++ board_game_detection/utils/object_detector.py
@@ -5,21 +5,20 @@ import numpy as np
 class ObjectDetector:
     #method that given corners of a rectangle on a image tilts it and rotates it so it is straight
     def perspective_transform(self, image, corners):
-        ordered_corners = corners
-        bottom_l, bottom_r, top_r, top_l = ordered_corners
+        bottom_left, bottom_right, top_right, top_left = corners
 
-        width_A = np.sqrt(((bottom_r[0] - bottom_l[0]) ** 2) + ((bottom_r[1] - bottom_l[1]) ** 2))
-        width_B = np.sqrt(((top_r[0] - top_l[0]) ** 2) + ((top_r[1] - top_l[1]) ** 2))
+        width_A = np.sqrt(((bottom_right[0] - bottom_left[0]) ** 2) + ((bottom_right[1] - bottom_left[1]) ** 2))
+        width_B = np.sqrt(((top_right[0] - top_left[0]) ** 2) + ((top_right[1] - top_left[1]) ** 2))
         width = max(int(width_A), int(width_B))
 
-        height_A = np.sqrt(((top_r[0] - bottom_r[0]) ** 2) + ((top_r[1] - bottom_r[1]) ** 2))
-        height_B = np.sqrt(((top_l[0] - bottom_l[0]) ** 2) + ((top_l[1] - bottom_l[1]) ** 2))
+        height_A = np.sqrt(((top_right[0] - bottom_right[0]) ** 2) + ((top_right[1] - bottom_right[1]) ** 2))
+        height_B = np.sqrt(((top_left[0] - bottom_left[0]) ** 2) + ((top_left[1] - bottom_left[1]) ** 2))
         height = max(int(height_A), int(height_B))
 
         dimensions = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1],
                         [0, height - 1]], dtype="float32")
 
-        ordered_corners = np.array(ordered_corners, dtype="float32")
+        ordered_corners = np.array(corners, dtype="float32")
 
         matrix = cv2.getPerspectiveTransform(ordered_corners, dimensions)
 
